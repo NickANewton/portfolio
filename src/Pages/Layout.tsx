efeacdc02424c73eea23f06b5c8be2c3f72fd871
Nick Newton <nickanewton@gmail.com>
2024-10-17 22:43:11 -0700

    Theming Prototyping/Icons

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,5 +1,9 @@
-import { Block, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { BuildingBlock } from "../Components/Block";
+import { SunIcon } from "../Icons/SunIcon";
+import { MoonIcon } from "../Icons/MoonIcon";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Layout() {
   const blockArray = [
@@ -7,23 +11,44 @@ export default function Layout() {
     { color: "bg-amber-500", connector: [1, 1, 1, 1], text: "Projects" },
     { color: "bg-cyan-700", connector: [1, 1, 1, 1], text: "Contact" },
   ];
+
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
+
   return (
     <div className="h-[100vh] w-[100vw] flex flex-col">
-      <header className="h-28 bg-[rgb(1,2,5)] text-white font-mono flex w-full px-6 gap-10 items-center">
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-bold">NICK NEWTON</h1>
-          <h2 className="text-xl orbitron-sub">Full Stack Web Dev</h2>
+      <header
+        className={twMerge(
+          "h-28 text-white font-mono flex justify-between w-full px-6 gap-20 items-center",
+          theme === "dark"
+            ? "bg-[rgb(1,2,5)] text-[rgb(211,211,211)]"
+            : "bg-gray-400 text-black"
+        )}
+      >
+        <div className="flex gap-20 items-center">
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold">NICK NEWTON</h1>
+            <h2 className="text-xl orbitron-sub">Full Stack Web Dev</h2>
+          </div>
+          <div className="flex gap-20 items-center pt-2">
+            {blockArray.map((block) => (
+              <button>
+                <BuildingBlock
+                  color={block.color}
+                  connector={block.connector}
+                  text={block.text}
+                />
+              </button>
+            ))}
+          </div>
         </div>
-        <div className="flex gap-8 items-center pt-2">
-          {blockArray.map((block) => (
-            <button>
-              <BuildingBlock
-                color={block.color}
-                connector={block.connector}
-                text={block.text}
-              />
-            </button>
-          ))}
+        <div className="flex">
+          <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            {theme === "dark" ? (
+              <SunIcon className="h-10 w-10 stroke-white" />
+            ) : (
+              <MoonIcon className="h-8 w-8 stroke-white" />
+            )}
+          </button>
         </div>
       </header>
       <Outlet />
